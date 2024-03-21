2599260265ffaf0e1c1149c01a19a21c746f0997
verybest123 <dima.sherbakov.2005.44@gmail.com>
2024-03-21 18:55:12 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+void printI(int summa, std::vector<int>& arr){
+    if (!summa){
+        for (int i=0;i<arr.size();i++){
+            std::cout<<arr[i]<<'\t';
+        }
+        std::cout<<'\n';
+    }
+    for (int i=1;i<=summa;i++){
+        if (arr.empty() || arr.back()<=i){
+            arr.push_back(i);
+            printI(summa-i,arr);
+            arr.pop_back();
+        }
+    }
+}
+
+void print(int summa){
+    std::vector<int> arr;
+    printI(summa,arr);
+}
+
+int main(){
+    print(5);
+    return 0;
+}
